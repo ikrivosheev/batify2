581b91e4639930094549cf1940f19ea74f85c4b7
Ivan Krivosheev <py.krivosheev@gmail.com>
2020-05-31 12:46:26 +0300

    Add error propagate

--- src/battery.c
+++ src/battery.c
@@ -13,13 +13,13 @@ static gboolean _get_sysattr_string(
     GError** error)
 {
     gchar *sys_filename;
-    gboolean sys_status;
+    gboolean result;
 
     sys_filename = g_build_filename(path, sys_attr, NULL);
-    sys_status = g_file_get_contents(sys_filename, value, NULL, error);
+    result = g_file_get_contents(sys_filename, value, NULL, error);
     g_free(sys_filename);
 
-    return sys_status;
+    return result;
 }
 
 static gboolean _get_sysattr_int(
@@ -29,64 +29,75 @@ static gboolean _get_sysattr_int(
     GError** error)
 {
     gchar* s_value;
-    gboolean sys_status;
+    gboolean result;
 
-    sys_status = _get_sysattr_string(path, sys_attr, &s_value, error);
-    if (sys_status == TRUE) 
-    {
-        gint64 tmp = g_ascii_strtoull(s_value, NULL, 10);
-        if (tmp != NULL)
-            *value = tmp;
-        else
-            sys_status = FALSE;
-        
-        g_free(s_value);
-    }
+    result = _get_sysattr_string(path, sys_attr, &s_value, error);
+    if (result == FALSE)
+        return FALSE;
 
-    return sys_status;
+    errno = 0;
+    gint64 tmp = g_ascii_strtoull(s_value, NULL, 10);
+    g_free(s_value);
+    if (errno)
+        return FALSE;
+    *value = tmp;
+
+    return TRUE;
 }
 
 gboolean get_battery_status(const gchar* sys_path, BATTERY_STATUS* status, GError** error)
 {
     gboolean result;
+    GError* _error = NULL;
     gchar* sys_status;
 
-    result = _get_sysattr_string(sys_path, BATTERY_STATUS_FILENAME, &sys_status, error);
-    
-    if (result == TRUE)
+    result = _get_sysattr_string(sys_path, BATTERY_STATUS_FILENAME, &sys_status, &_error);
+    if (result == FALSE)
     {
-        if (g_str_has_prefix(sys_status, "Charging") == TRUE)
-            *status = CHARGING_STATUS;
-        else if (g_str_has_prefix(sys_status, "Discharging") == TRUE)
-            *status = DISCHARGING_STATUS;
-        else if (g_str_has_prefix(sys_status, "Not charging") == TRUE)
-            *status = NOT_CHARGING_STATUS;
-        else if (g_str_has_prefix(sys_status, "Full") == TRUE)
-            *status = CHARGED_STATUS;
-        else
-            *status = UNKNOWN_STATUS;
-        
-        g_free(sys_status);
+        g_propagate_error(error, _error);
+        return FALSE;
     }
-    return result;
+    
+    if (g_str_has_prefix(sys_status, "Charging") == TRUE)
+        *status = CHARGING_STATUS;
+    else if (g_str_has_prefix(sys_status, "Discharging") == TRUE)
+        *status = DISCHARGING_STATUS;
+    else if (g_str_has_prefix(sys_status, "Not charging") == TRUE)
+        *status = NOT_CHARGING_STATUS;
+    else if (g_str_has_prefix(sys_status, "Full") == TRUE)
+        *status = CHARGED_STATUS;
+    else
+        *status = UNKNOWN_STATUS;
+    
+    g_free(sys_status);
+    return TRUE;
 }
 
 gboolean get_battery_capacity(const gchar* sys_path, guint64* capacity, GError** error)
 {
     gboolean result;
+    GError* _error = NULL;
     guint64 now, full;
 
-    result = _get_sysattr_int(sys_path, BATTERY_CAPACITY_FILENAME, capacity, error);
+    result = _get_sysattr_int(sys_path, BATTERY_CAPACITY_FILENAME, capacity, NULL);
     if (result == TRUE)
+    {
         return TRUE;
+    }
 
-    result = _get_sysattr_int(sys_path, BATTERY_CHARGE_NOW_FILENAME, &now, error);
+    result = _get_sysattr_int(sys_path, BATTERY_CHARGE_NOW_FILENAME, &now, &_error);
     if (result == FALSE)
+    {
+        g_propagate_error(error, _error);
         return FALSE;
+    }
 
-    result = _get_sysattr_int(sys_path, BATTERY_CHARGE_FULL_FILENAME, &full, error);
+    result = _get_sysattr_int(sys_path, BATTERY_CHARGE_FULL_FILENAME, &full, &_error);
     if (result == FALSE)
+    {
+        g_propagate_error(error, _error);
         return FALSE;
+    }
     
     if (full == 0)
         return FALSE;
@@ -98,20 +109,29 @@ gboolean get_battery_capacity(const gchar* sys_path, guint64* capacity, GError**
 gboolean get_battery_time(const gchar* sys_path, BATTERY_STATUS status, guint64* seconds, GError** error)
 {
     gboolean result;
+    GError* _error;
     guint64 charge_now, charge_full, current_now;
     
-    result = _get_sysattr_int(sys_path, BATTERY_CHARGE_NOW_FILENAME, &charge_now, error);
+    result = _get_sysattr_int(sys_path, BATTERY_CHARGE_NOW_FILENAME, &charge_now, &_error);
     if (result == FALSE)
+    {
+        g_propagate_error(error, _error);
         return FALSE;
+    }
     
-    result = _get_sysattr_int(sys_path, BATTERY_CHARGE_FULL_FILENAME, &charge_full, error);
+    result = _get_sysattr_int(sys_path, BATTERY_CHARGE_FULL_FILENAME, &charge_full, &_error);
     if (result == FALSE)
+    {
+        g_propagate_error(error, _error);
         return FALSE;
+    }
 
-    result = _get_sysattr_int(sys_path, BATTERY_CURRENT_NOW_FILENAME, &current_now, error);
-
+    result = _get_sysattr_int(sys_path, BATTERY_CURRENT_NOW_FILENAME, &current_now, &_error);
     if (result == FALSE)
+    {
+        g_propagate_error(error, _error);
         return FALSE;
+    }
     
     if (current_now <= 0)
         return FALSE;
